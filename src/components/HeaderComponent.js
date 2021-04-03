86b0a85b8a7f41deda19076a42c8dc11ea6fbf1b
Rachit <rachitagrawal1011@gmail.com>
2021-04-04 00:55:05 +0530

    third commit

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+
 
 class Header extends Component {
 
   constructor(props) {
     super(props);
 
-    this.toggleNav = this.toggleNav.bind(this);
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      isModalOpen:false
     };
+
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
   }
 
   toggleNav() {
@@ -18,6 +26,20 @@ class Header extends Component {
       isNavOpen: !this.state.isNavOpen
     });
   }
+
+  toggleModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+
+}
 
   render() {
     return (
@@ -41,6 +63,13 @@ class Header extends Component {
                   <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                 </NavItem>
               </Nav>
+
+              <Nav className="ml-auto" navbar >
+                <NavItem>
+                  <Button color="danger" outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"> Login</span></Button>
+                </NavItem>
+              </Nav>
+
             </Collapse>
 
           </div>
@@ -55,6 +84,35 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>
+            Login Form
+          </ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">UserName</Label>
+                <Input type="text" id="username" name="username" innerRef={(input)=>this.username = input} />
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password" innerRef={(input)=>this.password = input} />
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                    <Input type="checkbox" name="remember" innerRef = {(input) => this.remember = input} /> Remember Me
+                </Label>
+              </FormGroup>
+
+              <Button type="submit" value="submit" color = "primary">Login</Button>
+
+            </Form>
+          </ModalBody>
+        </Modal>
+
       </div>
     );
   }
--- src/shared/leaders.js
+++ src/shared/leaders.js
@@ -29,7 +29,7 @@ export const LEADERS = [
     {
       id: 3,
       name: 'Alberto Somayya',
-      image: '/assets/images/BestOne.jpg',
+      image:'/assets/images/alberto.png',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
